f5a7c3163829b68d064917cf841c8cea5cfa6798
JaySebagh <jaysebagh@gmail.com>
2019-10-16 12:17:32 -0700

    completed day 1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -35,16 +35,44 @@ class BinarySearchTree:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # check if self.value is already the target
+        if self.value == target:
+            return True
+        
+        # if the target is less than self.value, go left
+        if target < self.value:
+            # if there's no more left, return False
+            if not self.left:
+                return False
+            # else keep going using recursion
+            else:
+                return self.left.contains(target)
+        # if the target is more than self.value, go right
+        else:
+            # if there is no more right, return False
+            if not self.right:
+                return False
+            # else keep going using recursion
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # if you can't go right anymore, return the value
+        if not self.right:
+            return self.value
+        # else keep going right, using recursion
+        return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
@@ -66,10 +94,10 @@ class BinarySearchTree:
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
-    # Print In-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
+    # # Print In-order recursive DFT
+    # def pre_order_dft(self, node):
+    #     pass
 
-    # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
+    # # Print Post-order recursive DFT
+    # def post_order_dft(self, node):
+    #     pass
